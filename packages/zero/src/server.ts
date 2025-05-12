2bfa99d3b61f2d9f0cb7792cc1b519097ebd2943
pgvr <patrickvongoeler@gmail.com>
2025-05-12 22:49:58 +0200

    wip

--- packages/zero/src/server.ts
+++ packages/zero/src/server.ts
@@ -3,17 +3,22 @@ import { exec } from "node:child_process";
 
 const app = express();
 app.get("/run-migration", (req, res) => {
-	exec("bunx zero-deploy-permissions -p schema.ts", (error, stdout, stderr) => {
-		if (error) {
-			console.error(`Error: ${error}`);
-			return res.status(500).json({ error: error.message });
-		}
-		if (stderr) {
-			console.error(`stderr: ${stderr}`);
-		}
-		console.log(`stdout: ${stdout}`);
-		res.json({ message: "Migration completed", output: stdout });
-	});
+	const options = { cwd: process.cwd() };
+	exec(
+		"npx zero-deploy-permissions -p src/schema.ts",
+		options,
+		(error, stdout, stderr) => {
+			if (error) {
+				console.error(`Error: ${error}`);
+				return res.status(500).json({ error: error.message });
+			}
+			if (stderr) {
+				console.error(`stderr: ${stderr}`);
+			}
+			console.log(`stdout: ${stdout}`);
+			res.json({ message: "Migration completed", output: stdout });
+		},
+	);
 });
 
 const port = process.env.PORT || 3000;
